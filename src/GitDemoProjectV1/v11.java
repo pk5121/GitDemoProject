f38ceb34b4026bca1ff61de56df798ecab1e8df1
pk5121 <apraveenkumar51@gmail.com>
2022-02-23 11:30:49 +0530

    commited

---
+++ src/GitDemoProjectV1/v11.java
@@ -0,0 +1,47 @@
+package GitDemoProjectV1;
+
+public class v11 {
+
+	public static void main(String[ ] args) {
+		// TODO Auto-generated method stub
+		String hun = "y";
+		if(hun == "y")
+			System.out.println("eat something......");
+		else {
+			System.out.println("Do work.......");
+		}
+
+	}
+
+}
+
+public class test01 {
+
+	public static void main(String[ ] args) {
+		// TODO Auto-generated method stub
+		String hun = "y";
+		if(hun == "y")
+			System.out.println("eat something......");
+		else {
+			System.out.println("Do work.......");
+		}
+
+	}
+
+};
+
+public class v11 {
+
+	public static void main(String[ ] args) {
+		// TODO Auto-generated method stub
+		String hun = "y";
+		if(hun == "y")
+			System.out.println("eat something......");
+		else {
+			System.out.println("Do work.......");
+		}
+
+	}
+
+}
+
